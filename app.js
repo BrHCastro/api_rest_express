10501a41009b4d9dbd163c08a3b3babdd2b3910b
Henrique de Castro <hendecastro@gmail.com>
2021-05-29 18:27:56 -0300

    Segurança da API com jwt

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = process.env.KEY_API;
 
 const app = express();
 
@@ -11,7 +14,37 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//MiddleWere---------------------------------------------------------------------------------------------
+function auth(req, res, next) {
+    const authToken = req.headers['authorization'];
+    
+    if (authToken !== undefined) {
 
+        const bearer = authToken.split(' ');
+        let token = bearer[1];
+
+        jwt.verify(token, jwtSecret, (err, data) => {
+            if (err) {
+                res.status(401);
+                res.json({err: err});
+            } else {
+
+                req.token = token;
+                req.loggedUder = {
+                    id: data.id,
+                    email: data.email
+                }
+                next();
+            }
+        });
+
+    } else {
+        res.status(401);
+        res.json({err: 'Token inválido!'})
+    }
+}
+
+//Database-----------------------------------------------------------------------------------------------
 const connection = new Sequelize(process.env.DATABASE,process.env.USER_NAME,process.env.PASS, {
     host: process.env.HOST,
     dialect: 'mysql',
@@ -33,16 +66,32 @@ const Game = connection.define('games', {
     }
 });
 
-app.get('/games', (req, res) => {
+const User = connection.define('users', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+//Routers-------------------------------------------------------------------------------------------------
+app.get('/games', auth, (req, res) => {
     Game.findAll().then(games => {
         res.statusCode = 200;
-        res.json(games);
+        res.json({user: req.loggedUder, games: games});
     }).catch(err => {
         res.sendStatus(417);
     });
 });
 
-app.get('/game/:id', (req, res) => {
+app.get('/game/:id', auth, (req, res) => {
     if (isNaN(req.params.id)) {
         res.sendStatus(400);
     } else {
@@ -65,7 +114,7 @@ app.get('/game/:id', (req, res) => {
     }
 });
 
-app.post('/game', (req, res) => {
+app.post('/game', auth, (req, res) => {
     let {title, year, price} = req.body;
 
     Game.create({
@@ -80,7 +129,7 @@ app.post('/game', (req, res) => {
 
 });
 
-app.delete('/game/:id', (req, res) => {
+app.delete('/game/:id', auth, (req, res) => {
     if (isNaN(req.params.id)) {
         res.sendStatus(400);
     } else {
@@ -103,7 +152,7 @@ app.delete('/game/:id', (req, res) => {
     }
 });
 
-app.put('/game/:id', (req, res) => {
+app.put('/game/:id', auth, (req, res) => {
     if (isNaN(req.params.id)) {
         res.sendStatus(400);
     } else {
@@ -122,7 +171,61 @@ app.put('/game/:id', (req, res) => {
             res.sendStatus(404);
         });
     }
-})
+});
+
+app.post('/user', (req, res) => {
+    let {name, email, password} = req.body;
+
+    User.create({
+        name: name,
+        email: email,
+        password: password
+    }).then(() => {
+        res.sendStatus(200);
+    }).catch((err) => {
+        res.sendStatus(417);
+    })
+
+});
+
+app.post('/auth', (req, res) => {
+    let {email, password} = req.body;
+
+    if (email != undefined) {
+        if (password != undefined) {
+
+            User.findOne({
+                where: {email:email, password:password}
+            }).then(user => {
+
+                if (user) {
+
+                    jwt.sign({id:user.id,email: user.email}, jwtSecret,{expiresIn:'48h'}, (err, token) => {
+                        if(err) {
+                            res.status(400);
+                            res.json({err: err})
+                        } else {
+                            res.status(200);
+                            res.json({ok: "Usuário autenticado!", token: token});
+                        }
+                    })
+                } else {
+                    res.sendStatus(404);
+                }
+
+            }).catch(err => {
+                res.json({err: err})
+            });
+
+        } else {
+            res.status(400);
+            res.json({err: "Senha enviada é inválida!"})
+        }
+    } else {
+        res.status(400);
+        res.json({err: "E-mail enviado é inválido!"})
+    }
+});
 
 
 
